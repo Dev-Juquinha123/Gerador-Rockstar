91c15eb336145765211ad48514fe32856d9263ae
thiago carlos <50salasffcs1234@gmail.com>
2025-02-11 20:20:27 -0300

    Hard but become easy

--- Main.cpp
+++ Main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 
-int bitsBaixos(int valuebits);
-
+int bitsAltos(int bits);
 
 int main() {
-   
-    int valuebts;
-    unsigned int result;
-    std::cout << "Digite um valor inteiro: ";
-    std::cin >> valuebts;
-   
-    result = bitsBaixos(valuebts);
-    std::cout << "Os 16 bits mais altos desse valor correspondem ao numero: " << result << std::endl;
+ 
+    unsigned int valorinteiro;
+    int result;
+
+    std::cout << "Digite um valor inteiro ";
+    std::cin >> valorinteiro;
+    result = bitsAltos(valorinteiro);
+    std::cout << "Os 16 bits mais altos desse valor correspondem ao numero " << result << "." << std::endl;
 
     return 0;
 }
 
-int bitsBaixos(int bits)
+int bitsAltos(int bitss)
 {
-    int resultado = bits & ((1 << 16) - 1);  // Máscara para isolar os n bits
+    // 32 bits [0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0]
+    // bitss vai comparar os bits do numero bitss com a expressao
+    // ((1 << 16)  Move um único bit 1 para a posição 16 
+    // - 1 Subtrai 1, o que preenche os 16 bits menos significativos com 
+    // Isso desloca os 16 bits 1 para as posições de 16 a 31 (16 + 16 = 32)
+    // >> 16 voltamos com bits modificados 1011001010110000 para posicao original e temos novo numero
+
+    unsigned int resultado = (bitss & (((1 << 16) - 1) << 16)) >> 16;
+
 
     return resultado;
 }
