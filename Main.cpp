2c2d47fe9f19f9b48d84d51809fbc7b678ca8548
thiago carlos <50salasffcs1234@gmail.com>
2025-02-11 16:12:22 -0300

    Deobfuscador c++

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 
+char codificar(char cactere);
+char decodificar(char othercharacter);
 
 
 int main() {
-  
-    std::cout << "Maça = \t\t 20 Unidades" << std::endl;
-    std::cout << "Melancia = \t 50 Unidades" << std::endl;
-    std::cout << "Ameixa = \t 30 Unidades" << std::endl;
+    char valueuser;
+    char showcharacter;
+
+    std::cout << "Digite um caractere: ";
+    std::cin >> valueuser;
+    std::cout << "Carectere escolhido: " << valueuser << std::endl;
+    showcharacter = codificar(valueuser);
+    std::cout << "New value " << showcharacter << std::endl;
+    showcharacter = decodificar(valueuser);
+    std::cout << "New value decompiled " << showcharacter << std::endl;
+
     return 0;
+}
+
+char codificar(char cactere)
+{
+    int cacterenw;
+    cacterenw = cactere + 3;
+
+    return cacterenw;
+}
+
+char decodificar(char crts)
+{
+    int value;
+    value = crts - 3;
+
+    return value;
 }
