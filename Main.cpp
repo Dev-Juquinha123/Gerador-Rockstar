9b9f05af792eeaea0399de52c08a2b5cea0839b0
thiago carlos <50salasffcs1234@gmail.com>
2025-02-11 13:28:31 -0300

    conhencendo tabela ascii

--- Main.cpp
+++ Main.cpp
@@ -1,60 +1,27 @@
 #include <iostream>
-#include <climits>
-#include <limits.h>
 
-
-bool isShort(int);
-bool isInt(int);
+char charada(char value, int numericoancii);
 
 int main() {
-    int numbertest;
-    std::cout << "Digite um valor inteiro: ";
-    std::cin >> numbertest;
+  
+    int numerocaracter;
+    char receive;
+    char newvarible;
 
+    std::cout << "Digite um ch: ";
+    std::cin >> receive;
+    std::cout << "Distancia de: ";
+    std::cin >> numerocaracter;
 
-    if (isShort(numbertest))
-    {
-        std::cout << numbertest << " Cabe em 16 bits" << std::endl;
-
-    }
-    else {
-        std::cout << numbertest << " Nao Cabe em 16 bits\a" << std::endl;
-    }
-
-    if (isInt(numbertest)) {
-        std::cout << numbertest << " Cabe em 32 bits" << std::endl;
-    }
-    else {
-        std::cout << numbertest << " Nao Cabe em 32 bits\a" << std::endl;
-    }
-    
-    
+    newvarible = charada(receive, numerocaracter);
+    std::cout << "Seu numero na tabela ascii 5 a frente e: " << newvarible << std::endl;
     return 0;
 }
 
-bool isInt(int number1)
+char charada(char caracter , int codigonumerico)
 {
-    if (number1 > INT_MIN && number1 < INT_MAX){
-        return true;
-    }
-    else {
-        return false;
-    }
-        
-   
-
-}
-
-bool isShort(int number)
-{
-
-    if (number > SHRT_MIN && number < SHRT_MAX){
-        return true;
-    }
-    else {
-        return false;
-    }
-
-    
-
+    // Calcular distancia
+    int novaposicao;
+    novaposicao = codigonumerico + caracter;
+    return novaposicao;
 }
