ed019d684361fdc2d84f53060fb9c27f9befa1aa
thiago carlos <50salasffcs1234@gmail.com>
2025-02-12 16:22:14 -0300

    Conversao de tipos

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
 
-int bitsAltos(int bits);
 
 int main() {
- 
-    unsigned int valorinteiro;
-    int result;
 
-    std::cout << "Digite um valor inteiro ";
-    std::cin >> valorinteiro;
-    result = bitsAltos(valorinteiro);
-    std::cout << "Os 16 bits mais altos desse valor correspondem ao numero " << result << "." << std::endl;
+    double x1;
+    double x2;
+    int newvalue;
+
+    std::cout << "Digite valor para x1: ";
+    std::cin >> x1;
+    std::cout << std::endl;
+    std::cout << "Digite valor para x2: ";
+    std::cin >> x2;
+    newvalue = (int)x1 + (int)x2;
+    std::cout << "A adicao inteira dos valores e " << newvalue;
+
 
     return 0;
 }
 
-int bitsAltos(int bitss)
-{
-    // 32 bits [0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0][0]
-    // bitss vai comparar os bits do numero bitss com a expressao
-    // ((1 << 16)  Move um único bit 1 para a posição 16 
-    // - 1 Subtrai 1, o que preenche os 16 bits menos significativos com 
-    // Isso desloca os 16 bits 1 para as posições de 16 a 31 (16 + 16 = 32)
-    // >> 16 voltamos com bits modificados 1011001010110000 para posicao original e temos novo numero
-
-    unsigned int resultado = (bitss & (((1 << 16) - 1) << 16)) >> 16;
-
-
-    return resultado;
-}
