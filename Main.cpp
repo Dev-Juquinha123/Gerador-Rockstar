59656bb063132442c8004570b23071e9768bed95
thiago carlos <50salasffcs1234@gmail.com>
2025-02-13 14:26:48 -0300

    Conversao de tipos

--- Main.cpp
+++ Main.cpp
@@ -2,19 +2,15 @@
 
 int main() {
 
-    int horario;
-   
-
-    std::cout << "Entre com o numero de segundos: ";
-    std::cin >> horario;
+    double vzlsf = 245.795;
+    double receiveint = (int)vzlsf;
     
-    int dias = horario / 86400;         // 1 dia tem 86400 segundos (24 * 60 * 60)
-    int horas = (horario % 86400) / 3600; // O resto da divisão por 86400 dá o tempo restante, e dividimos por 3600 para obter horas
-    int minutos = (horario % 3600) / 60;  // O resto da divisão por 3600 dá os segundos restantes, dividimos por 60 para obter minutos
-    int segundos = horario % 60;         // O resto da divisão por 60 dá os segundos restantes
+    receiveint = receiveint * 100;
+    vzlsf = vzlsf * 100;
+    std::cout << "New value: " << receiveint << std::endl;
 
+    std::cout << "New value2: " << vzlsf << std::endl;
 
-    std::cout << horario << " Segundos = " << dias << " dias, " << horas << " horas, " << minutos << " minutos e " << segundos << " segundos";
     return 0;
 }
 
