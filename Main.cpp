b6e7137735f06092c03866b2ac21955efaae069f
thiago carlos <50salasffcs1234@gmail.com>
2025-02-15 16:49:59 -0300

    Pequena analise de string com vetor inicializado automaticamente

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
+#include <cstring>
 
 int main() {
-    const int tam = 15;
-    char name[tam];
-    char lastname[tam];
-    std::cout << "Primeiro nome? ";
-    std::cin >> name;
-    std::cout << "Segundo nome? ";
-    std::cin >> lastname;
-    std::cout << std::endl;
-    std::cout << "Bom dia, " << name << " " << lastname << "." << std::endl;
-    std::cout << "Seja bem vindo " << name[0] << lastname[0];
-  
+    char nome[] = "c++ Primer Plus";
+    std::cout << "Tamanho do vetor e: " << strlen(nome) << std::endl;
+    std::cout << "Tamanho de bityes e: " << sizeof(nome);
     return 0;
 }
