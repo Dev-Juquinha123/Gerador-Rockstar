0a45a5b30b176b6c5fd207b4596d617df4750741
thiago carlos <50salasffcs1234@gmail.com>
2025-02-14 22:26:08 -0300

    recebendo valores do usuario

--- Main.cpp
+++ Main.cpp
@@ -1,42 +1,30 @@
 #include <iostream>
 
 int main() {
-    int vet[5] = { 15, 11, 39, 42, 57 };
-
-    // Exibir o vetor
-    std::cout << "Vetor atual: ";
-    for (int i = 0; i < 5; i++) {
-        std::cout << vet[i] << " ";
-    }
-
+    
+    double precoproduto[3] = { 1.50, 2.00, 1.00 };
+    double quantidadedesejada[3] = {};
+    std::cout << "Prezado Cliente," << std::endl;
+    std::cout << "Digite a quantidade de quilos desejados: " << std::endl;
+    std::cout << "Alface: ";
+    std::cin >> quantidadedesejada[0];
+    std::cout << "Beterraba: ";
+    std::cin >> quantidadedesejada[1];
+    std::cout << "Cenoura: ";
+    std::cin >> quantidadedesejada[2];
     std::cout << std::endl;
-
-    // Pedir ao usuário qual posição deseja alterar
-    int alterarpos;
-    std::cout << "Alterar posicao (0 a 4): ";
-    std::cin >> alterarpos;
-
-    // Verificar se a posição é válida
-    if (alterarpos < 0 || alterarpos >= 5) {
-        std::cout << "Posicao invalida!" << std::endl;
-    }
-    else {
-        // Exibir o valor atual e pedir um novo valor
-        std::cout << "Valor atual na posicao " << alterarpos << ": " << vet[alterarpos] << std::endl;
-        std::cout << "Novo valor: ";
-        int novovalor;
-        std::cin >> novovalor;
-
-        // Alterar o valor no vetor
-        vet[alterarpos] = novovalor;
-
-        // Exibir o vetor atualizado
-        std::cout << "Vetor atualizado: ";
-        for (int i = 0; i < 5; i++) {
-            std::cout << vet[i] << " ";
-        }
-        std::cout << std::endl;
-    }
-
+    double valortotal;
+    std::cout << "Resumo da Compra" << std::endl;
+    std::cout << "-----------------" << std::endl;
+    valortotal = precoproduto[0] * quantidadedesejada[0];
+    std::cout << "Alface\t = R$" << valortotal << std::endl;
+    valortotal = precoproduto[1] * quantidadedesejada[1];
+    std::cout << "Beterraba\t = R$" << valortotal << std::endl;
+    valortotal = precoproduto[2] * quantidadedesejada[2];
+    std::cout << "Cenoura\t = R$" << precoproduto[2] * quantidadedesejada[2] << std::endl;
+    std::cout << "-----------------";
+    std::cout << std::fixed;
+    std::cout.precision(2);
+    std::cout << "Total: \t=  R$" <<(precoproduto[0] * quantidadedesejada[0] + precoproduto[1] * quantidadedesejada[1] + precoproduto[2] * quantidadedesejada[2]);
     return 0;
 }
