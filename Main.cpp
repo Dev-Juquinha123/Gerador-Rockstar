4276d98c2b47eb6c791c2665afa67bbac35f2fef
thiago carlos <50salasffcs1234@gmail.com>
2025-02-18 18:42:03 -0300

    Recebendo dados de um jogador

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstring>
 
 struct jogador
 {
@@ -9,12 +8,13 @@ struct jogador
 };
 
 void exibir(jogador);
+jogador ler();
 
 
 int main()
 {
-
-	jogador romario = { "Romario", 200000, 1000 };
+	std::cout << "Digite os dados do jogador: " << std::endl;
+	jogador romario = ler();
 	exibir(romario);
 
 	return 0;
@@ -25,4 +25,17 @@ void exibir(jogador j)
 	std::cout << "Nome: " << j.nome << std::endl;
 	std::cout << "Salario: " << j.salario << std::endl;
 	std::cout << "Gols: " << j.gols << std::endl;
+}
+
+jogador ler()
+{
+	jogador j;
+	std::cout << "Nome: ";
+	std::cin.getline(j.nome, 40);
+	std::cout << "Salario: ";
+	std::cin >> j.salario;
+	std::cout << "Gols: ";
+	std::cin >> j.gols;
+	std::cin.ignore();
+	return j;
 }
