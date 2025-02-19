9bd62d47a135c72d559573cb54ee3cf23730a0f0
thiago carlos <50salasffcs1234@gmail.com>
2025-02-19 20:30:32 -0300

    New base registros

--- Main.cpp
+++ Main.cpp
@@ -1,38 +1,24 @@
 #include <iostream>
 #include <string>
 
-struct Palavra
+
+struct Livro
 {
-	std::string portugues;
-	std::string ingles;
-	std::string espanhol;
+	unsigned paginas;
+	unsigned capitulos;
 };
 
+struct jogo
+{
+	float vida;
+	float colete;
+	unsigned inventario;
+};
 
 
 int main()
 {
 
-	Palavra dicionario[10] = {
-		{"Laranja", "Orange", "Naranja"},
-		{"Maca", "Apple", "Manzana"},
-	};
-
-	std::cout << "Entre com mais uma palavra: " << std::endl;
-	std::cout << "Portugues; ";
-	std::cin >> dicionario[2].portugues;
-	std::cout << "Ingles; ";
-	std::cin >> dicionario[2].ingles;
-	std::cout << "Espanhol; ";
-	std::cin >> dicionario[2].espanhol;
-	std::cout << "Mostrando toda lista" << std::endl;
-	std::cout << "" << std::endl;
-	for (int i = 0; i < 10; i++)
-	{
-		std::cout << dicionario[i].portugues << "\n";
-		std::cout << dicionario[i].ingles;
-		std::cout << dicionario[i].espanhol;
-	}
 
    
 
