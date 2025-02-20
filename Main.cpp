532f6a66790cacdb9a337f1c3d7fcb77ee2a9f99
thiago carlos <50salasffcs1234@gmail.com>
2025-02-20 16:58:42 -0300

    Mostrando tamanho da uniao

--- Main.cpp
+++ Main.cpp
@@ -1,40 +1,18 @@
 #include <iostream>
-#include <string>
-#include <sstream>
 
 
-struct data
+
+union indetificador
 {
-    unsigned int dia;
-    unsigned int mes;
-    unsigned int ano;
+    char ch;
+    int num;
+    double frac;
 };
-
-struct horario
-{
-    unsigned int hora;
-    unsigned int minutos;
-    unsigned int segundos;
-};
-
-struct  evento
-{
-    data encontro;
-    horario timeevento;
-	char local[50];
-};
-
 
 int main() {
     
-    evento pascoa;
-
-	std::cout << "Nome do evento: "; std::cin >> pascoa.local;
-	std::cout << std::endl;
-	std::cout << "Inicio horario: "; std::cin >> pascoa.timeevento.hora; std::cout << ":"; std::cin >> pascoa.timeevento.minutos; std::cout << ":"; std::cin >> pascoa.timeevento.segundos;
-    std::cout << "Data: "; std::cin >> pascoa.encontro.dia; std::cout << ":"; std::cin >> pascoa.encontro.mes; std::cout << ":"; std::cin >> pascoa.encontro.ano;
-	std::cout << pascoa.local << " - " << pascoa.timeevento.hora << ":" << pascoa.timeevento.minutos << ":" << pascoa.timeevento.segundos << " - " << pascoa.encontro.dia << "/" << pascoa.encontro.mes << "/" << pascoa.encontro.ano << std::endl;
-
+	indetificador id;
+	std::cout << sizeof(id) << std::endl;
 
 
     return 0;
