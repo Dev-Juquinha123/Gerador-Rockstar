ccf5b53f5049bf7e6f5b42a0f9df15e1c636dd37
thiago carlos <50salasffcs1234@gmail.com>
2025-02-21 11:02:50 -0300

    Sobrecarga de registros

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,87 @@
 #include <iostream>
+#include <iomanip>
+#include <cstring>
 
-enum mes { SEG, TER, QUAR, QUINT, SEX };
+
+struct Horario
+{
+	unsigned int hora;
+	unsigned int minuto;
+};
+
+// Sobrecarga do operador >> para Horario
+std::istream& operator>>(std::istream& in, Horario& h) {
+	in >> h.hora >> h.minuto;
+	return in;
+}
+
+// Sobrecarga do operador << para Horario
+std::ostream& operator<<(std::ostream& out, const Horario& h) {
+	out << h.hora << ":" << h.minuto;
+	return out;
+}
+
+struct Descricao
+{
+	char desc[18];
+};
+
+// Sobrecarga do operador >> para Descricao
+std::istream& operator>>(std::istream& in, Descricao& d) {
+	in.ignore(); // Remove caracteres pendentes no buffer
+	in.getline(d.desc, sizeof(d.desc)); // Lê uma linha inteira com espaços
+	return in;
+}
+
+// Sobrecarga do operador << para Descricao
+std::ostream& operator<<(std::ostream& out, const Descricao& d) {
+	out << d.desc;
+	return out;
+}
+
+
+struct Data
+{
+	unsigned int dia;
+	unsigned int mes;
+	unsigned int ano;
+
+	Horario horario;
+	Descricao descricao;
+};
+
 
 
 int main() {
     
-	char meses[5][10] = 
-    {
-        "Segunda", "Terca", "Quarta", "Quinta", "sexta",
-	};
-
-	for (mes ind = SEG; ind <= SEX; ind = mes(ind +1))
+	Data data[10];
+	
+	std::cout << "Entre com 2 eventos: " << std::endl;
+	std::cout << "#1 " << std::endl;
+	for (size_t i = 0; i < 2; i++)
 	{
-		std::cout << meses[ind] << std::endl;
+		std::cout << "Data: ";
+		std::cin >> data[i].dia;
+		std::cin >> data[i].mes;
+		std::cin >> data[i].ano;
+		std::cout << std::endl;
+		std::cout << "Hora: ";
+		std::cin >> data[i].horario;
+		std::cout << std::endl;
+		std::cout << "Desc: ";
+		std::cin >> data[i].descricao;
+		std::cout << "#2 " << std::endl;
 	}
+	
+	//
+	std::cout << "--------------------" << std::endl;
+	std::cout << "Eventos Cadastrados" << std::endl;
+	for (size_t i = 0; i < 2; i++) {
+		std::cout << data[i].dia << "/" << data[i].mes << ":" << data[i].ano << " "
+			<< data[i].horario << " "
+			<< data[i].descricao << std::endl;
+	}
+
 
     return 0;
 }
