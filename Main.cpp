f32c117a2111b56633847e04bb81d24c7f45ec29
thiago carlos <50salasffcs1234@gmail.com>
2025-02-22 19:21:28 -0300

    No read

--- Main.cpp
+++ Main.cpp
@@ -1,29 +1,10 @@
 #include <iostream>
 
 
-struct balao
-{
-    float diametro;  // diametro em metros
-    char marca[20];  // nome da marca
-    int modelo;      // numero do modelo
-};
 
 int main() {
 
-    balao* ptr = new balao;
-
-    std::cout << "Entre com os dados: " << std::endl;
-    std::cout << "Diametro: ";
-    std::cin >> ptr->diametro;
-    std::cout << std::endl;
-    std::cout << "Marca: ";
-    std::cin >> ptr->marca;
-    std::cout << "Modelo: ";
-    std::cin >> ptr->modelo;
-    std::cout << "Conteudo diametro: " << ptr->diametro << "\n";
-    std::cout << "Conteudo marca: " << ptr->marca << "\n";
-    std::cout << "Conteudo modelo: " << ptr->modelo << "\n";
-    delete ptr;
+    std::cout << "Discord.gg/community123" << std::endl;
 
     return 0;
 }
