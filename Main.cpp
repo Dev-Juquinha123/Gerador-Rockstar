baff4a3a9eb858420448ba8200c7ae1c2425184c
thiago carlos <50salasffcs1234@gmail.com>
2025-02-22 19:53:24 -0300

    Alocacao dinamica

--- Main.cpp
+++ Main.cpp
@@ -4,8 +4,13 @@
 
 int main() {
 
-    int* ptr = (int*)0x01;
-    std::cout << *ptr;
+
+	int* ptr = new int(100);
+
+	std::cout << "Conteudo armazendo: " << *ptr << std::endl;
+	std::cout << "Digite novo valor para esse bloco de memoria: ";
+	std::cin >> *ptr;
+	std::cout << *ptr << " 0x" <<  ptr << std::endl;
 
     return 0;
 }
