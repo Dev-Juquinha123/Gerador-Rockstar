9813c9cbf88edd8f951a4e2bebbfbcef438aa6e4
thiago carlos <50salasffcs1234@gmail.com>
2025-02-23 18:29:35 -0300

    For aninhado

--- Main.cpp
+++ Main.cpp
@@ -2,24 +2,28 @@
 #include "functions.h"
 
 int main() {
+    int numlinhas;
+    int j;
+    std::cout << "Entre com o numero de linhas: ";
+    std::cin >> numlinhas;
+    std::cout << std::endl;
 
-    char vetorlista[10];
-    // Parte crescente
-    for (int i = 1; i <= 5; i++) {
+
+    // Colunas
+    for (int i = 1; i <= numlinhas; i++) {
+        // Imprimir pontos (4)
+        for (int j = 0; j < numlinhas - i; j++) {
+            std::cout << ".";
+        }
+
+        // Imprimir asteriscos
         for (int j = 0; j < i; j++) {
             std::cout << "*";
         }
+
         std::cout << std::endl;
     }
 
-    // Parte decrescente
-    for (int i = 4; i >= 1; i--) {
-        for (int j = 0; j < i; j++) {
-            std::cout << "*";
-        }
-        std::cout << std::endl;
-    }
-   
 
     // Arquivo .h incluso
     std::cout << std::endl;
