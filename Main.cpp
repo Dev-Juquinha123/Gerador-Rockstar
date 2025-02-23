7130eeb708342eb49949aa945e15eac56deafd29
thiago carlos <50salasffcs1234@gmail.com>
2025-02-23 00:16:21 -0300

    lacos para vetores

--- Main.cpp
+++ Main.cpp
@@ -1,29 +1,13 @@
 #include <iostream>
 
 int main() {
-    int vtam;
-    std::cout << "Quantos valores deseja guardar: ";
-    std::cin >> vtam;
 
-    int* ptrvet = new int[vtam];
-    std::cout << std::endl;
-    std::cout << "Quais valores? ";
-
-    for (int contador = 0; contador < vtam; contador++) {
-        std::cin >> ptrvet[contador];
+    int B[10] = {32, 16, 47, 10, 82, 29, 30, 28, 15, 64};
+	
+    for (size_t i = 0, j = 9; i < 5; i++, j--) {
+        std::cout << " [" << B[i] << ", " << B[j] << "]";
     }
 
-    // Exibe todos os valores armazenados em uma única mensagem
-    std::cout << "Os valores armazenados sao: ";
-    for (int is = 0; is < vtam; is++) {
-        std::cout << ptrvet[is];
-        if (is < vtam - 1) {  // Adiciona vírgula entre os valores, exceto no último
-            std::cout << ", ";
-        }
-    }
-    std::cout << "Foram armazenados";
-    std::cout << std::endl;
-
-    delete[] ptrvet;  // Libera a memória alocada
+	
     return 0;
 }
