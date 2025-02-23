6db31f8c1469a339ea6c8e2cf07adafdb4950aa5
thiago carlos <50salasffcs1234@gmail.com>
2025-02-23 00:46:41 -0300

    Versao 2.0 com ponteiros para vetor em loop

--- Main.cpp
+++ Main.cpp
@@ -3,9 +3,19 @@
 int main() {
 
     int A[10] = {46,78, 40, 96, 74, 58, 32, 56, 91, 6};
+    int (*ptr)[10] = &A;
 
+    std::cout << "Address: " << "0x" << ptr << std::endl;
+    std::cout << "[1] Normal" << std::endl;
     for (size_t i = 0, j = 1; i < 9; i = i = i +2, j = j +2) {
+        
         std::cout << " [" << A[i] << ", " << A[j] << "]";
+    }
+    std::cout << std::endl;
+    std::cout << "[2] Ponteiro" << std::endl;
+    for (size_t iptr = 0, jptr = 1; iptr < 9; iptr = iptr = iptr + 2, jptr = jptr + 2) {
+
+        std::cout << " [" << (*ptr)[iptr] << ", " << (*ptr)[jptr] << "]";
     }
 
 	
