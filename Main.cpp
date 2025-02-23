717a2cee2e508450388031366ffd3e3242f2d22c
thiago carlos <50salasffcs1234@gmail.com>
2025-02-23 16:30:40 -0300

    testando valores por incremento

--- Main.cpp
+++ Main.cpp
@@ -3,21 +3,11 @@
 
 int main() {
 
-    int A[10] = {46,78, 40, 96, 74, 58, 32, 56, 91, 6};
-    int (*ptr)[10] = &A;
-
-    std::cout << "Address: " << "0x" << ptr << std::endl;
-    std::cout << "[1] Normal" << std::endl;
-    for (size_t i = 0, j = 1; i < 9; i = i = i +2, j = j +2) {
-        
-        std::cout << " [" << A[i] << ", " << A[j] << "]";
+    for (size_t i = 1; i < 65; i = i *2)
+    {
+        std::cout << i << std::endl;
     }
-    std::cout << std::endl;
-    std::cout << "[2] Ponteiro" << std::endl;
-    for (size_t iptr = 0, jptr = 1; iptr < 9; iptr = iptr = iptr + 2, jptr = jptr + 2) {
 
-        std::cout << " [" << (*ptr)[iptr] << ", " << (*ptr)[jptr] << "]";
-    }
     // Arquivo .h incluso
     std::cout << std::endl;
     std::cout << "By " << NOME << std::endl;
