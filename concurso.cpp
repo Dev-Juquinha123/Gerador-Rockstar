a9ca4fab144e719c38ff3e4765ca5d3a861f4e94
thiago carlos <50salasffcs1234@gmail.com>
2025-02-27 15:42:19 -0300

    Atualizando prototipos

--- concurso.cpp
+++ concurso.cpp
@@ -6,6 +6,7 @@
 void Drawline(char item, int vezesdwl) {
 	for (size_t i = 0; i < vezesdwl; i++)
 	{
-		std::cout << item << std::endl;
+		std::cout << item;
 	}
+	std::cout << std::endl;
 }
--- concurso.h
+++ concurso.h
@@ -2,3 +2,21 @@
 
 // Prototipos de funcoes e Registros
 void Drawline(char item, int vezesdwl);
+
+struct Horario
+{
+	int hora;
+	int minuto;
+};
+
+struct Questoes
+{
+	int Graudificuldade;
+	int inicio;
+	int fim;
+};
+
+struct Media
+{
+
+};
