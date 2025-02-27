08d19504040e16a6e5e89be6d218ae061f326baf
thiago carlos <50salasffcs1234@gmail.com>
2025-02-27 16:09:20 -0300

    Iniciando vetor dinamico de participantes

--- estatisticas.cpp
+++ estatisticas.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include "concurso.h"
 
-// Apenas Main
+
 int main() {
    
 	Drawline('*', 50);
 	std::cout << "Concurso de Programacao" << std::endl;
-	
+	int Nparticipantes;
+	int Nquestoes;
+
+	// Coletando informacoes
+	std::cout << "Qual o numero de participantes? ";
+	std::cin >> Nparticipantes;
+	std::cout << "Qual o numero de questoes? ";
+	std::cin >> Nquestoes;
+
+	// Vetor dinamico onde guarda
+	Questoes* vetDnQ = new Questoes[Nparticipantes];
 
 	Drawline('*', 50);
     return 0;
