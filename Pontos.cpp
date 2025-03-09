bbd5b2eacb32ba9e217ad504e8a465359eead6c9
thiago carlos <50salasffcs1234@gmail.com>
2025-03-09 16:08:04 -0300

    Call functions

--- Pontos.cpp
+++ Pontos.cpp
@@ -5,9 +5,13 @@
 
 int main() {
 
-	// Variavel do tipo Jogo
+	
 	Coord ponto;
 	ponto.MoveTo(10, 10);
+	std::cout << "(" << ponto.x << ", " << ponto.y << ")" << std::endl;
+
+	ponto.Translate(2, 5);
+	std::cout << "(" << ponto.x << ", " << ponto.y << ")" << std::endl;
 
 	return 0;
 }
