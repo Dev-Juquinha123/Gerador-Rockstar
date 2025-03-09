ce85cf2654f995231b74af0a67360f88f8d1c0b9
thiago carlos <50salasffcs1234@gmail.com>
2025-03-09 17:34:03 -0300

    Defining size

--- Coord.cpp
+++ Coord.cpp
@@ -5,9 +5,16 @@
 // Criar retangulo
 void Rect::Create(int ax, int ay, int bx, int by)
 {
+	// Definiu tamanho
+	a.a = ax;
+	a.b = ay;
+	b.a = bx;
+	b.b = by;
 }
 
 // Movimentar retangulo
 void Rect::Translate(int dx, int dy)
 {
+	a.a += dx;
+	a.b += dy;
 }
