982808593ef40c3068fdfc3aedd099f880304320
thiago carlos <50salasffcs1234@gmail.com>
2025-03-10 17:44:13 -0300

    changing comments

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include "Rect.h"
 
-
 int main()
 {
     Rect r;
 
-    // Cria o retângulo com coordenadas (0, 0) e (10, 10)
+    // Criando um retângulo com coordenadas (0,0) e (10,10)
     r.Create(0, 0, 10, 10);
-    r.Print();  // Mostra as coordenadas dos pontos
+    std::cout << "Retângulo inicial:\n";
+    r.Print();
 
-    // Mover o retângulo
-    r.Translate(5, 5);  // Move os pontos para a direita e para cima
-    r.Print();  // Mostra as novas coordenadas dos pontos
-    std::cout << std::endl;
+    // Movendo o retângulo 5 unidades para a direita e 5 para cima
+    r.Translate(5, 5);
+    std::cout << "\nApós a translação:\n";
+    r.Print();
 
-
+    return 0;
 }
