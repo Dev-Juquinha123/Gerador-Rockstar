b703502c1cda9f70faa462d6c705c030b2cfb646
thiago carlos <50salasffcs1234@gmail.com>
2025-03-11 18:10:03 -0300

    updating constructor

--- Jogo.cpp
+++ Jogo.cpp
@@ -1,44 +1,40 @@
+#include "Jogo.h"
 #include <iostream>
-#include "Rect.h"
+using namespace std;
 
-Rect::Rect() : valid(false) {}
+// -----------------------------------------------
+// Definição da Classe Jogo
+// -----------------------------------------------
 
-void Rect::Create(int x1, int y1, int x2, int y2) {
-    this->x1 = x1;
-    this->y1 = y1;
-    this->x2 = x2;
-    this->y2 = y2;
-    valid = (x1 != x2 && y1 != y2);
+Jogo::Jogo(const string& titulo, float valor)
+{
+    nome = titulo;
+    preco = valor;
+    horas = 0;
+    custo = preco;
 }
 
-bool Rect::IsValid() const {
-    return valid;
+void Jogo::atualizar(float valor)
+{
+    preco = valor;
+    calcular();
 }
 
-void Rect::Print() const {
-    if (!valid) {
-        std::cout << "Retângulo inválido!\n";
-        return;
-    }
-    std::cout << "Cantos: (" << x1 << "," << y1 << ") até (" << x2 << "," << y2 << ")\n";
-    std::cout << "Largura: " << (x2 - x1) << ", Altura: " << (y2 - y1) << "\n";
+void Jogo::jogar(int tempo)
+{
+    horas = horas + tempo;
+    calcular();
 }
 
-void Rect::Translate(int dx, int dy) {
-    if (!valid) return;
-    x1 += dx;
-    y1 += dy;
-    x2 += dx;
-    y2 += dy;
+void Jogo::exibir()
+{
+    cout << fixed;
+    cout.precision(2);
+
+    cout << nome << " R$"
+        << preco << " "
+        << horas << "h = R$"
+        << custo << "/h\n";
 }
 
-void Rect::Scale(double factor) {
-    if (!valid || factor <= 0) return;
-    x2 = x1 + static_cast<int>((x2 - x1) * factor);
-    y2 = y1 + static_cast<int>((y2 - y1) * factor);
-}
-
-int Rect::GetArea() const {
-    if (!valid) return 0;
-    return (x2 - x1) * (y2 - y1);
-}
+// -----------------------------------------------
