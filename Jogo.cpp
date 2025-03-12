e1322dda52fd1277f33dbf938406c7d6a7778108
thiago carlos <50salasffcs1234@gmail.com>
2025-03-12 19:23:30 -0300

    fix function exibir

--- Jogo.cpp
+++ Jogo.cpp
@@ -34,6 +34,7 @@ void Atleta::exibir() const {
     std::cout << std::fixed;
     std::cout.precision(2);
     std::cout << "Percentual: " << percentual << "%\n";
+    std::cout << std::endl;
 }
 
 // Função amiga
