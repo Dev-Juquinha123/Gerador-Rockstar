0bab8756e3e0bb98d825919deb60a523128e605d
thiago carlos <50salasffcs1234@gmail.com>
2025-03-12 18:22:29 -0300

    preparing functions

--- Jogo.cpp
+++ Jogo.cpp
@@ -1,6 +1,26 @@
 #include "Jogo.h"
 #include <iostream>
 
+void calcular(Atleta& atl)
+{
+	if (atl.tentativas != 0)
+		atl.percentual = 100.0f * atl.acertos / atl.tentativas;
+	else
+		atl.percentual = 0
+}
 
+void exibir(const Atleta& atl)
+{
+	std::cout << "Acertos: " << atl.acertos << "  ";
+	std::cout << "Tentativas: " << atl.tentativas << "  ";
+	std::cout << " Percentual: " << atl.percentual << "\n";
+}
 
+Atleta& acumular(Atleta& soma, const Atleta& atl)
+{
+	soma.tentativas += atl.tentativas;
+	soma.acertos += atl.acertos;
+	calcular(soma);
+	return soma;
+}
 
--- Jogo.h
+++ Jogo.h
@@ -1,3 +1,26 @@
 #include <string>
 using std::string;
 
+class Atleta
+{
+public:
+	// Construtor & Destrutor
+	Atleta();
+	~Atleta();
+
+private:
+	// Atributos
+	int acertos;
+	int tentativas;
+	float percentual;
+};
+
+//
+Atleta::Atleta()
+{
+}
+
+
+Atleta::~Atleta()
+{
+}
