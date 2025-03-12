0bee5cd9d1fa468433fc643754196e28f830fffa
thiago carlos <50salasffcs1234@gmail.com>
2025-03-12 18:13:06 -0300

    reset

--- Temporario.cpp
+++ Temporario.cpp
@@ -1,11 +1,8 @@
 #include "Jogo.h"
 #include <iostream>
-using namespace std;
+
 
 int main()
 {
-	const Jogo ratchet = Jogo("Ratchet & Clank", 150.0f);
-	/*ratchet.atualizar(125.0f);
-	ratchet.jogar(5);*/
-	ratchet.exibir();
+
 }
