038383732e48a45cedbdea1e9f3797e81db46d00
thiago carlos <50salasffcs1234@gmail.com>
2025-04-07 09:37:17 -0300

    remove handler error

--- MessageHandler.cpp
+++ MessageHandler.cpp
@@ -50,15 +50,6 @@ void MessageHandler::saveMessageToFile(const std::string& message) {
 
 /// Função para carregar mensagens de um arquivo
 void MessageHandler::loadMessagesFromFile() {
-    // Erro handler
-    if (!fileExists("messages.txt")) {
-        std::cout << "Arquivo 'messages.txt' não encontrado\n";
-
-        std::cout << "Tente novamente.\n";
-
-        return;
-
-    }
 
     std::ifstream file("messages.txt");
 
