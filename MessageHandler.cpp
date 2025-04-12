75664c095540e38ab5d9a772e1d8c60c2506e37f
thiago carlos <50salasffcs1234@gmail.com>
2025-04-12 15:21:40 -0300

    OBS #1

--- MessageHandler.cpp
+++ MessageHandler.cpp
@@ -24,6 +24,7 @@ void MessageHandler::printMessage(const std::string& message, int count) {
     for (int i = 0; i < count; i++) {
         std::cout << message << std::endl;
         std::cout << "discord.gg/community123";
+
     }
 }
 
